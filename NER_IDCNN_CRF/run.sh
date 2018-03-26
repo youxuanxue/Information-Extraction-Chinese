a562557f4a43a0384b1212b307a77a294dcc7d25
Jiao Xue <xuejiao@wandoujia.com>
2018-03-26 09:53:07 +0800

    Add test_file in run.sh

--- NER_IDCNN_CRF/run.sh
+++ NER_IDCNN_CRF/run.sh
@@ -1,17 +1,18 @@
 #!/usr/bin/env bash
 
 python main.py \
-	--train true \
-	--clean true \
-	--ckpt_path /data1/xuejiao/model/ner/ckpt \
-	--summary_path /data1/xuejiao/model/ner/summary \
-	--log_file /data1/xuejiao/model/ner/train.log \
-	--map_file /data1/xuejiao/model/ner/maps.pkl \
-	--vocab_file /data1/xuejiao/model/ner/vocab.json \
-	--result_path /data1/xuejiao/model/ner/result/  \
-	--emb_file /data1/xuejiao/model/ner/vec.txt \
-	--pre_emb false \
-	--tag_schema iob \
-	--max_epoch 50 \
-	--model_type idcnn \
-	--train_file /data1/xuejiao/data/match/corpus.txt
+	--train=True \
+	--clean=True \
+	--ckpt_path=/data1/xuejiao/model/ner/ckpt \
+	--summary_path=/data1/xuejiao/model/ner/summary \
+	--log_file=/data1/xuejiao/model/ner/train.log \
+	--map_file=/data1/xuejiao/model/ner/maps.pkl \
+	--vocab_file=/data1/xuejiao/model/ner/vocab.json \
+	--result_path=/data1/xuejiao/model/ner/result/  \
+	--emb_file=/data1/xuejiao/model/ner/vec.txt \
+	--pre_emb=False \
+	--tag_schema=iob \
+	--max_epoch=50 \
+	--model_type=idcnn \
+	--train_file=/data1/xuejiao/data/match/corpus.txt \
+    --test_file=/data1/xuejiao/data/ops/header_ops_592_corpus.txt
